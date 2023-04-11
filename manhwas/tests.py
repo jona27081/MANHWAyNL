db720481e522379a8f9bc348c89a44b3d0d88b5c
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-04-11 13:03:01 -0500

    update unit-test

--- manhwas/tests.py
+++ manhwas/tests.py
@@ -25,8 +25,18 @@ MANHWAS_QUERY = '''
 }
 '''
 
+CREATE_MANHWA_MUTATION = '''
+mutation createManhwas($titulo: String, $genero: String, $descripcion: String, $autor: String, $estado: String, $capitulos: Int, $artista: String, $pais: String, $clasificacion: String, $adaptacion: String) {
+    createManhwas(titulo: $titulo, genero: $genero, descripcion: $descripcion, autor: $autor, estado: $estado, capitulos: $capitulos, artista: $artista, pais: $pais, clasificacion: $clasificacion, adaptacion: $adaptacion) {
+      titulo
+    }
+}
+'''
+
+
 class ManhwaTestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
+
     def setUp(self):
         self.manhwa1 = mixer.blend(Manhwas)
         self.manhwa2 = mixer.blend(Manhwas)
@@ -36,13 +46,28 @@ class ManhwaTestCase(GraphQLTestCase):
             MANHWAS_QUERY,
         )
 
-
         content = json.loads(response.content)
-        #print(content)
+        # print(content)
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
-        print ("query manhwas results ")
-        print (content)
+        print("query manhwas results ")
+        print(content)
         assert len(content['data']['manhwas']) == 2
 
 
+    def test_createManhwa_mutation(self):
+
+        response = self.query(
+            CREATE_MANHWA_MUTATION,
+            variables={'titulo': 'The God of High School', 'genero': 'Acción, Aventura, Comedia, Fantasía',
+                      'descripcion': 'Jin Mo-Ri, un estudiante de preparatoria y experto en artes marciales', 'autor': 'Park Yong-Je',
+                      'estado': 'Completo', 'capitulos': 517, 'artista': 'Park Yong-Je', 'pais': 'Corea del Sur',
+                      'clasificacion': 'Mayores de 14 años', 'adaptacion': 'Webtoon'}
+        )
+        print('mutation ')
+        print(response)
+        content = json.loads(response.content)
+        print(content)
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual( 
+            {"createManhwas": {"titulo": "The God of High School"}}, content['data'])
