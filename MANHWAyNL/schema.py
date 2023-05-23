f4353d61f9ccea9c8c30e654cdbafb2f24ce6636
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-05-23 15:14:17 -0500

    add authentication

--- MANHWAyNL/schema.py
+++ MANHWAyNL/schema.py
@@ -1,12 +1,19 @@
 import graphene
+import graphene
+import graphql_jwt
+
+# ...code
 
 import manhwas.schema
+import users.schema
 
 
-class Query(manhwas.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, manhwas.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(manhwas.schema.Mutation, graphene.ObjectType):
-    pass
+class Mutation(users.schema.Mutation, manhwas.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
